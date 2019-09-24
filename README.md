44e0420f82d686a31b96be726e0c1e7b98310a44
Luca Freschi <luca_freschi@hms.harvard.edu>
2019-09-24 15:26:43 -0400

    fixed setup.py, updated README.md (installation)

--- README.md
+++ README.md
@@ -1,18 +1,28 @@
 # Objective
-Quickly call lineages using different SNP schemes from vcf files. The package will be developed primarily to call TB lineages, but if you build your own SNP scheme you can use it to call lineages for vistually any bacterial species.
+Quickly call lineages using different SNP schemes from vcf files. The package will be developed primarily to call MTB (Mycobacterium tuberculosis) lineages, but if you build your own SNP scheme you can use it to call lineages for vistually any bacterial species.
 
-# Requirements
+
+
+# Installation
+```
+git clone https://github.com/farhat-lab/fast-lineage-caller-vcf.git
+cd fast-lineage-caller-vcf/
+python setup.py sdist
+
+```
 
 
 # Example
 I try to assign the lineage to the isolate SAMEA1708322:
 ```
-./fast-lineage-caller-vcf.py ~/mfarhat/rollingDB/genomic_data/SAMEA1708322/pilon/SAMEA1708322.vcf ./snp_schemes/coll.tsv
+fast-lineage-caller-vcf ~/mfarhat/rollingDB/genomic_data/SAMEA1708322/pilon/SAMEA1708322.vcf ./snp_schemes/coll.tsv
 ```
 
 # Changelog (roadmap)
+
 Version 0.1.1
-- 
+- working python module
+- updated README (installation section)
 
 Version 0.1.0
 - initial python module (not tested)
--- setup.py
+++ setup.py
@@ -1,30 +1,26 @@
 from setuptools import setup
 
 def readme():
-	    with open('README.rst') as f:
-		            return f.read()
+    with open('README.rst') as f:
+        return f.read()
 
-		    setup(name='metatools_ncbi',
-				    version='0.1.4',
-				    description='Download biosample and SRA runinfo metadata from NCBI',
-				    url='http://github.com/farhat-lab/metatools_ncbi',
-				    author='Luca Freschi',
-				    author_email='l.freschi@gmail.com',
-				    license='LGPLv3',
-				    packages=['metatools_ncbi'],
-				    classifiers=[
-					            'Development Status :: 4 - Beta',
-						            'Environment :: Console',
-							            'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-								            'Operating System :: POSIX :: Linux',
-									            'Programming Language :: Python :: 3',
-										            'Topic :: Scientific/Engineering :: Bio-Informatics',
-											          ],
-				          keywords='ncbi download metadata biosample runinfo',
-					        install_requires=[
-							          'progress',
-								            'requests'
-									          ],
-						      scripts=['bin/metatools_download'],
-						      zip_safe=False)
-
+setup(name='fast_lineage_caller_vcf',
+version='0.1.1',
+description='Module to call MTB lineages from a .vcf file',
+url='https://github.com/farhat-lab/fast-lineage-caller-vcf/',
+author='Luca Freschi',
+author_email='l.freschi@gmail.com',
+license='LGPLv3',
+packages=[],
+classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+      ],
+      keywords='Mycobacterium tuberculosis lineage calling',
+      install_requires=[],
+      scripts=['bin/fast-lineage-caller-vcf'],
+zip_safe=False)
